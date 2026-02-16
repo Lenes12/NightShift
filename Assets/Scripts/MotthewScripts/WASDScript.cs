3f19bbff691815d2a7a0ccbea964e756ce7c6ff0
Lenes12 <jewel032401@gmail.com>
2026-02-15 20:02:29 -0700

    WASD Script Implemented

---
+++ Assets/Scripts/MotthewScripts/WASDScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class WASDScript : MonoBehaviour
+{
+    private Vector3 PlayerPosition;
+
+    [Header("Player Settings")]
+    public float PlayerSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PlayerPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.wKey.isPressed == true)
+        {
+            PlayerPosition.y += PlayerSpeed / 95;
+        }
+        if (Keyboard.current.sKey.isPressed == true)
+        {
+            PlayerPosition.y -= PlayerSpeed / 95;
+        }
+        if (Keyboard.current.dKey.isPressed == true)
+        {
+            PlayerPosition.x += PlayerSpeed / 95;
+        }
+        if (Keyboard.current.aKey.isPressed == true)
+        {
+            PlayerPosition.x -= PlayerSpeed / 95;
+        }
+        this.transform.position = PlayerPosition;
+    }
+}
