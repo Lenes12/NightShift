e27c6d7aa543ba9b90a053d23ab84dee4a768629
Lenes12 <jewel032401@gmail.com>
2026-03-01 19:57:14 -0700

    Added Object CounterUI

--- Assets/Scripts/MotthewScripts/PlayerScript.cs
+++ Assets/Scripts/MotthewScripts/PlayerScript.cs
@@ -1,16 +1,41 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 3f;
+    private Rigidbody rb;
+
+    public TextMeshProUGUI scoreText;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        scoreText.text = "Ingredients Collected: " + ScoreManagerWin.totalScore;
     }
 
-    // Update is called once per frame
-    void Update()
+     void FixedUpdate()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(h,v,0.0f);
+        transform.Translate(movement * speed * Time.deltaTime);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag == "Collectables")
+        {
+            ScoreManagerWin.totalScore += 1;
+            scoreText.text = "Ingredients Collected:  " + ScoreManagerWin.totalScore;
+            collision.gameObject.SetActive(false);
+        }
     }
 }
