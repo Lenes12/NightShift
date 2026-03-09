d608dd7da6ef5a5025543add65bc6b09e6d6ad49
Lenes12 <jewel032401@gmail.com>
2026-03-09 16:54:13 -0600

    Changed Button Controls and added them to disappear after

--- Assets/Scripts/ButtonPressScript.cs
+++ Assets/Scripts/ButtonPressScript.cs
@@ -1,31 +1,57 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
 
-public class Button1PressScript : MonoBehaviour
-  
+public class ButtonPressScript : MonoBehaviour
 {
     public GameObject GateObject;
+    public GameObject Button;
 
+    private bool playerNear = false;
 
     void Start()
     {
-
         if (GateObject != null)
         {
             GateObject.SetActive(true);
-
         }
-       
     }
 
-    void OnMouseDown()
+    void Update()
+    {
+        if (playerNear && Input.GetKeyDown(KeyCode.E))
         {
             if (GateObject != null)
             {
                 GateObject.SetActive(false);
+                Destroy(Button);
             }
         }
-        
-    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNear = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (GateObject != null)
+        {
+            GateObject.SetActive(false);
+        }
+    }
 }
